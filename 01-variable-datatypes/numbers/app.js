93832d3698e04850aad6eba51b40ab326a6003e5
Helix <starc.vinay44@gmail.com>
2024-02-19 01:15:12 +0530

    strings

---
+++ 01-variable-datatypes/numbers/app.js
@@ -0,0 +1,21 @@
+const n = new Number(600.8945);
+
+x = n.toString();
+
+x = n.toString().length;
+
+x = n.toFixed(2);
+
+x = n.toPrecision(4);
+
+x = n.toExponential(2);
+
+x = n.toLocaleString('en-US');
+
+x = n.valueOf();
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+
+console.log(x, typeof n);
